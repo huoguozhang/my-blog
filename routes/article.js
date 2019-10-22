c8cae196764568bfad5e7d552febf212d19b28f1
huoguozhang <zhanglijianwork@163.com>
2019-10-22 09:02:46 +0800

    i want delete

--- routes/article.js
+++ routes/article.js
@@ -44,16 +44,23 @@ const Routes = [
          WHERE (article.title LIKE '%${search}%' OR article.content LIKE '%${search}%' OR user.nickname LIKE '%${search}%')
       `)
       const totalCount = totalCountRow[0][0].count
-      const rows = await models.sequelize.query(`
-       SELECT article.updated_time, article.created_time, article.uid, article.title, article.summary, article.author,
-         user.uid AS 'user.uid',
-         user.avatar AS 'user.avatar', user.description AS 'user.description',
-         user.nickname AS 'user.nickname', user.username AS 'user.username',
-         user.created_time AS 'user.created_time', user.updated_time AS 'user.updated_time'
-         FROM article AS article LEFT
-         OUTER JOIN user AS user ON article.author = user.uid
-         WHERE (article.title LIKE '%${search}%' OR article.content LIKE '%${search}%' OR user.nickname LIKE '%${search}%') AND
-        (article.updated_time BETWEEN　2018-09-01 AND 2020-10-01)
+      // const rows = await models.sequelize.query(`
+      //  SELECT article.updated_time, article.created_time, article.uid, article.title, article.summary, article.author,
+      //    user.uid AS 'user.uid',
+      //    user.avatar AS 'user.avatar', user.description AS 'user.description',
+      //    user.nickname AS 'user.nickname', user.username AS 'user.username',
+      //    user.created_time AS 'user.created_time', user.updated_time AS 'user.updated_time'
+      //    FROM article AS article LEFT
+      //    OUTER JOIN user AS user ON article.author = user.uid
+      //    WHERE (article.title LIKE '%${search}%' OR article.content LIKE '%${search}%' OR user.nickname LIKE '%${search}%') AND
+      //   (article.updated_time BETWEEN　2018-09-01 AND 2020-10-01)
+      //    LIMIT ${request.query.limit}
+      //    OFFSET ${(request.query.page - 1) * request.query.limit}
+      // `)
+       const rows = await models.sequelize.query(`
+       SELECT article.updated_time, user.nickname FROM article    
+       LEFT OUTER JOIN user ON article.author = user.uid
+       WHERE (article.updated_time BETWEEN '2018-09-01 00:00:00' AND '2018-09-02 00:00:00') AND (article.title LIKE '%xx%' OR article.content LIKE '%xx%' OR user.nickname LIKE '%xx%') 
          LIMIT ${request.query.limit}
          OFFSET ${(request.query.page - 1) * request.query.limit}
       `)
